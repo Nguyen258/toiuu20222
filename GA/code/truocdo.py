831b422e8857a33aac3a2f2885511c75e88308f1
DongLieu <duongduydongtq@gmail.com>
2023-07-12 00:33:56 +0700

    doc file

--- GA/code/truocdo.py
+++ GA/code/truocdo.py
@@ -1,166 +1,52 @@
-    def init_Sol(self):
-        print(self.gv_hd)
-        def tim_xx(self):
-            while(1):
-                stores = []
-                for i in range(self.N):
-                    stores.append(i)
-                x = [-1]*self.N
-                k_x = {}
-                for i in range(self.K):
-                    k_x[i] = []
 
-                # chien luoc chon DA cung duoc GV cung hoi dong
-                for gv in range(self.M):
-                    gv_hd = copy.deepcopy(self.gv_hd[gv])
-                    if len(gv_hd) >= self.d:
-                        tmp = []
-                        while(len(gv_hd) > 0):
-                            rand_sl = random.randint(0, self.d - 1)
-                            if len(gv_hd) <= rand_sl:
-                                random_selection = gv_hd
-                            else:
-                                random_selection = random.sample(gv_hd, rand_sl)
-                            
-                            rand_hd = 0
-                            while(1):
-                                rand_hd = random.randint(0, self.K - 1)
-                                if rand_hd in tmp: continue
-                                if len(k_x[rand_hd]) + len(random_selection) > self.b: continue
-                                break
+# path_output,N,M,K,t,s,g,a,b,c,d,e,f
 
-                            tmp.append(rand_hd)
-                            k_x[rand_hd] = k_x[rand_hd] + random_selection
+# Nhập số nguyên từ bàn phím
+num = (input(""))
 
-                            for item in random_selection:
-                                stores.remove(item)
-                                gv_hd.remove(item)
-                        
-                print("33333",stores)
-                check_1 = 0
-                for i in range(self.K):
-                    l = len(k_x[i])
-                    if self.b - l >= 0:
-                        random_selection = random.sample(stores, self.b - l)
-                    if self.b - l <0:
-                        check_1 = 1
-                        break
-                    if self.b - l ==0:
-                        print("ok 1")
 
-                    for item in random_selection:
-                        stores.remove(item)
-                    
-                    k_x[i] = k_x[i] + random_selection
+data = num.split(" ")
 
-                for k, v in k_x.items():
-                    print(len(v))
+N = int(data[0])
+M = int(data[1])
+K = int(data[2])
 
-                if check_1 == 1:
-                    print("do check_1")
-                    continue
-                for key, value in k_x.items():
-                    for i in value:
-                        x[i] = key
+num = (input(""))
+data = num.split(" ")
+a = int(data[0])
+b = int(data[1])
+c = int(data[2])
+d = int(data[3])
+e = int(data[4])
+f = int(data[5])
 
-                check, gvhd = gv_tranh_hd(self, x)
-                if check:
-                    return x, k_x, gvhd
-                else:
-                    print("k_x:", k_x)
-                    print("x", x)
-                    print("do check 2")
-                    print(stores)
-                    continue
-                
-        def gv_tranh_hd(self, x):
-            t = self.t
-            gv_hd = {}
-            for i in range(self.M):
-                gv_hd[i] = []
+s = []
+for i in range(N):
+    s.append([-1 for k in range(N)])
 
-            for i in range(self.N):
-                gv = t[i] - 1
-                hd = x[i]
-                gv_hd[gv] = gv_hd[gv] +[hd]
-            
-            for i in range(self.M):
-                tmp = set(gv_hd[i])
-                gv_hd[i] = tmp 
+for i in range(N):
+    num = (input(""))
+    data = num.split(" ")
+    for j in range(N):
+        s[i][j] = int(data[j])
 
-                if len(tmp) >= self.d:
-                    print(self.d)
-                    print(tmp)
-                    return False, None
-            print(" 0 ")
-            return True, gv_hd
-                
+g = []
+for i in range(N):
+    g.append([-1 for k in range(M)])
 
-        def tim_x(self):
-            while(1):
-                x = []
-                k_x = {}
-                for i in range(self.K):
-                    k_x[i] = []
-                
-                for i in range(self.N):
-                    random_number = random.randint(0, self.K - 1)
-                    x.append(random_number)
-                    k_x[random_number] = k_x[random_number] + [i]
-                    
-                max = 0
-                for _, value in k_x.items():
-                    if len(value) < self.a: continue
-                    if len(value) > max: max = len(value)
+for i in range(N):
+    num = (input(""))
+    data = num.split(" ")
+    for j in range(M):
+        g[i][j] = int(data[j])
 
-                if max > self.b:
-                    # print("nono")
-                    continue
+num = (input(""))
+data = num.split(" ")
 
-                check, gvhd = gv_tranh_hd(self, x)
-                if check:
-                    return x, k_x, gvhd
-                else:
-                    continue
+t = [0]*N
+for i in range(N):
+    t[i] = int(data[i])
 
-        def tim_y(self, gvhd):
-            y = []
-            k_y = {}
-            for i in range(self.K):
-                k_y[i] = []
-
-            for gv in range(self.M):#gv i hoi dong nao
-                chon_gv_in = []
-                for i in range(self.K):
-                    if i in gvhd[gv]:continue
-                    chon_gv_in.append(i)
-
-                HD_gv = random.choice(chon_gv_in)
-                
-                y.append(HD_gv)
-                k_y[HD_gv] = k_y[HD_gv] + [gv]
-
-            for value in k_y.values():
-                if len(value) < self.c: 
-                    return False, None, None
-                if len(value) > self.d:
-                    return False, None, None
-
-            return True, y, k_y
-
-        while(1):
-            if self.b*self.K == self.N:
-                x, k_x, gvhd = tim_xx(self)
-                # print("done1")
-            else:
-                x, k_x, gvhd = tim_x(self)
-
-            suc, y, k_y = tim_y(self, gvhd) 
-            # print("done2")
-            if suc:
-                # print("pass")
-                self.x = x
-                self.k_x = k_x
-                self.k_y = k_y
-                self.y = y
-                break
+print(s)
+print(g)
+print(t)
